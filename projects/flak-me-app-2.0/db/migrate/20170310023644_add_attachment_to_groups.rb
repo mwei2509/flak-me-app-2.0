8e90abd709ee4eef7dac4808a521e0dd8e0b1ad8
mwei2509 <m2dl89@gmail.com>
2017-03-09 22:18:38 -0500

    database fix

--- projects/flak-me-app-2.0/db/migrate/20170310023644_add_attachment_to_groups.rb
+++
@@ -1,5 +0,0 @@
-class AddAttachmentToGroups < ActiveRecord::Migration[5.0]
-  def change
-    add_column :groups, :group_img, :attachment
-  end
-end
--- projects/flak-me-app-2.0/db/migrate/20170310030532_remove_group_img_from_groups.rb
+++
@@ -1,5 +0,0 @@
-class RemoveGroupImgFromGroups < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :groups, :group_img
-  end
-end
